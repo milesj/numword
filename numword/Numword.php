0b436f45856ab3e1f8e6cbfee6e82da5ea0c0fc8
Miles Johnson <mileswjohnson@gmail.com>
2011-04-27 22:35:52 -0700

    Adding basic locale support. Updating to 1.5 while fixing bugs.

--- numword/Numword.php
+++ numword/Numword.php
@@ -2,21 +2,21 @@
 /**
  * Numword - Converts a number to its word form.
  * 
- * @author 		Miles Johnson - www.milesj.me
- * @copyright	Copyright 2006-2009, Miles Johnson, Inc.
- * @license 	http://www.opensource.org/licenses/mit-license.php - Licensed under The MIT License
- * @link		www.milesj.me/resources/script/numword
+ * @author 		Miles Johnson - http://milesj.me
+ * @copyright	Copyright 2006-2011, Miles Johnson, Inc.
+ * @license 	http://opensource.org/licenses/mit-license.php - Licensed under The MIT License
+ * @link		http://milesj.me/resources/script/numword
  */
 
 class Numword {
 
 	/**
-	 * Current version: www.milesj.me/resources/logs/numword
+	 * Current version: http://milesj.me/resources/logs/numword
 	 *
 	 * @access public
 	 * @var int
 	 */
-	public $version = '1.4';
+	public $version = '1.5';
 
 	/**
 	 * Holds the basic numbers: 1-10
@@ -26,7 +26,7 @@ class Numword {
 	 * @static
 	 */
 	public static $digits = array('zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine');
-	
+
 	/**
 	 * Holds all the teens: 11-19
 	 *
@@ -45,7 +45,7 @@ class Numword {
 		18 => 'eighteen',
 		19 => 'nineteen'
 	);
-	
+
 	/**
 	 * Holds the multiples of ten: 10, 20, - 90
 	 *
@@ -64,7 +64,7 @@ class Numword {
 		80 => 'eighty',
 		90 => 'ninety'
 	);
-	
+
 	/**
 	 * Holds the larger numbers.
 	 *
@@ -83,21 +83,21 @@ class Numword {
 		8	=> 'sextillion',
 		9	=> 'septillion',
 		10	=> 'octillion',
-		11 	=> 'nonillion', 
+		11 	=> 'nonillion',
 		12 	=> 'decillion',
 		13	=> 'undecillion',
-		14	=> 'duodecillion', 
-		15	=> 'tredecillion', 
+		14	=> 'duodecillion',
+		15	=> 'tredecillion',
 		16	=> 'quattuordecillion',
 		17	=> 'quindecillion',
-		18	=> 'sexdecillion', 
-		19	=> 'septendecillion', 
+		18	=> 'sexdecillion',
+		19	=> 'septendecillion',
 		20	=> 'octodecillion',
-		21 	=> 'novemdecillion', 
+		21 	=> 'novemdecillion',
 		22	=> 'vigintillion',
 		23 	=> 'centillion'
 	);
-	
+
 	/**
 	 * The separator between words.
 	 *
@@ -105,8 +105,8 @@ class Numword {
 	 * @var string
 	 * @static
 	 */
-	public static $sep = '-'; 
-	
+	public static $sep = '-';
+
 	/**
 	 * Converts a single number to its word format.
 	 *
@@ -117,14 +117,14 @@ class Numword {
 	 */
 	public static function single($number) {
 		$numberClean = trim(str_replace(',', '', $number));
-		
+
 		if (is_numeric($numberClean)) {
 			return self::__convert($numberClean);
 		} else {
 			return $number;
 		}
 	}
-	
+
 	/**
 	 * Converts many numbers to its word format.
 	 *
@@ -139,10 +139,10 @@ class Numword {
 				$numbers[$index] = self::single($number);
 			}
 		}
-		
+
 		return $numbers;
 	}
-	
+
 	/**
 	 * Converts any numeric instance in a block of text (sentence/string) to its word format.
 	 *
@@ -153,33 +153,33 @@ class Numword {
 	 */
 	public static function block($string) {
 		$words = explode(' ', $string);
-		
+
 		foreach ($words as $index => $word) {
 			if (preg_match("/[0-9]/i", $word)) {
 				$fl = mb_substr($word, 0, 1);
 				$ll = mb_substr($word, -1);
 				$pre = '';
 				$suf = '';
-				
+
 				if (!is_numeric($fl)) {
 					$pre = $fl;
 					$word = mb_substr($word, 1, mb_strlen($word) - 1);
 				}
-				
+
 				if (!is_numeric($ll)) {
 					$suf = $ll;
 					$word = mb_substr($word, 0, mb_strlen($word) - 1);
 				}
-				
+
 				$words[$index] = $pre . self::__convert($word) . $suf;
 			} else {
 				$words[$index] = $word;
 			}
 		}
-		
+
 		return implode(' ', $words);
 	}
-	
+
 	/**
 	 * Converts american currency into its word format.
 	 *
@@ -188,23 +188,26 @@ class Numword {
 	 * @return string
 	 * @static
 	 */
-	public static function currency($number) {
-		$number = trim(str_replace(array('$', ','), '', $number));
-		$cents  = trim(mb_strstr($number, '.'), '.');
-		$amount = mb_substr($number, 0, mb_strpos($number, '.'));
-		
-		$return = self::__convert($amount) .' dollars';
-		
+	public static function currency($number, array $currency = array()) {
+		$locale = localeconv();
+		$currency = $currency + array(
+			'dollar' => 'dollar(s)',
+			'cent' => 'cent(s)'
+		);
+
+		$number = trim(str_replace(array($locale['currency_symbol'], $locale['mon_thousands_sep']), '', $number));
+		$cents  = trim(mb_strstr($number, $locale['mon_decimal_point']), $locale['mon_decimal_point']);
+		$amount = mb_substr($number, 0, mb_strpos($number, $locale['mon_decimal_point']));
+
+		$return = self::__convert($amount) .' '. $currency['dollar'];
+
 		if ($cents != '00' && mb_strlen($cents) == 2) {
-			$return .= ' and '. self::__convertDoubles($cents) .' cent';
-			if ($cents > 1) {
-				$return .= 's';
-			}
+			$return .= ' & '. self::__convertDoubles($cents) .' '. $currency['cent'];
 		}
-		
+
 		return $return;
 	}
-	
+
 	/**
 	 * Determines numbers length then converts to words.
 	 *
@@ -215,7 +218,7 @@ class Numword {
 	 */
 	private static function __convert($number) {
 		$length = mb_strlen($number);
-		
+
 		if ($length > 3) {
 			$return = self::__convertMultiples($number);
 		} else if ($length == 3) {
@@ -225,10 +228,10 @@ class Numword {
 		} else {
 			$return = self::$digits[$number];
 		}
-		
+
 		return $return;
 	}
-	
+
 	/**
 	 * Converts doubles: 10-99.
 	 *
@@ -240,7 +243,7 @@ class Numword {
 	private static function __convertDoubles($number) {
 		$fn = mb_substr($number, 0, 1);
 		$ln = mb_substr($number, -1);
-		
+
 		if ($fn == 1 && $ln != 0) {
 			$return = self::$teens[$number];
 		} else if ($ln == 0) {
@@ -248,10 +251,10 @@ class Numword {
 		} else {
 			$return = self::$tens[$fn .'0'] . self::$sep . self::$digits[$ln];
 		}
-		
+
 		return $return;
 	}
-	
+
 	/**
 	 * Converts triples: 100-999.
 	 *
@@ -264,47 +267,51 @@ class Numword {
 		$fn = mb_substr($number, 0, 1);
 		$ln = mb_substr($number, -2);
 		$return = '';
-		
+
 		if ($fn != 0) {
 			$return = self::$digits[$fn] . self::$sep . self::$exponents[1];
 		}
-		
+
 		if ($ln != '00') {
 			$return .= ' '. self::__convertDoubles($ln);
 		}
-		
+
 		return $return;
 	}
-	
+
 	/**
 	 * Converts large numbers: 1000+.
-	 * 
+	 *
 	 * @access private
 	 * @param int $number
 	 * @return string
 	 * @static
 	 */
 	private static function __convertMultiples($number) {
-		$numrev = mb_strrev($number);
+		$numrev = strrev($number);
 		$parts = str_split($numrev, 3);
-		
+
 		$cur = 1;
 		$newParts = array();
+
 		foreach ($parts as $index => $part) {
-			$ret = self::__convert(mb_strrev($part));
+			$ret = self::__convert(strrev($part));
+
 			if ($index > 0 && $part != '000') {
 				$ret .= self::$sep . self::$exponents[$cur];
 			}
+
 			if ($ret != '') {
 				$newParts[$index] = $ret;
 			}
+
 			$cur++;
 		}
-		
+
 		$parts = array_reverse($newParts);
 		$return = implode(', ', $parts);
-			
+
 		return $return;
 	}
-	
+
 }
